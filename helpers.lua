d0116cc6eaf80e2c7b43654e8d48c1432d8e8e20
Bruno Moura <brunomoura.meta@gmail.com>
2024-03-25 14:50:46 -0300

    feat: some new features.

---
+++ helpers.lua
@@ -0,0 +1,22 @@
+local M  = {}
+
+M.split_string = function(str, delimiter)
+  delimiter = delimiter or " "
+  local result = {}
+  local from = 1
+  local delim_from, delim_to = string.find(str, delimiter, from)
+  while delim_from do
+    table.insert(result, string.sub(str, from, delim_from-1))
+    from = delim_to + 1
+    delim_from, delim_to = string.find(str, delimiter, from)
+  end
+  table.insert(result, string.sub(str, from))
+  return result
+end
+
+M.parse_bin_path = function(str)
+  local tokens = M.split_string(str, "/")
+  return tokens[#tokens]
+end
+
+return M
--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local helpers = require("helpers")
 
 local config = {}
 
@@ -8,24 +9,6 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- Set the custom key bindings.
-config.keys = require("keys")
-config.colors = require("kanagawa_dragon")
-config.tab_bar_at_bottom = true
-config.audible_bell = "Disabled"
-config.use_fancy_tab_bar = false
-config.enable_scroll_bar = false
-config.freetype_load_flags = "NO_HINTING"
-config.font_size = 13
-
-config.font = wezterm.font({
-  family = "JetBrains Mono",
-  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-  weight = "Medium"
-})
-
-config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-
 wezterm.on(
   "format-tab-title",
   -- tab, tabs, panes, config, hove, max_width
@@ -34,25 +17,36 @@ wezterm.on(
     local program_name = pane.foreground_process_name
     if program_name == "" then
       -- If no foreground process name is detected, default to 'bash' or another default
-      program_name = "bash"
+      return "bash"
     end
 
-    -- Similar to string.split in other languages.
-    local function parse_bin_path(str, delimiter)
-      local result = {}
-      local from = 1
-      local delim_from, delim_to = string.find(str, delimiter, from)
-      while delim_from do
-        table.insert(result, string.sub(str, from, delim_from-1))
-        from = delim_to + 1
-        delim_from, delim_to = string.find(str, delimiter, from)
-      end
-      table.insert(result, string.sub(str, from))
-      return result[#result]
-    end
-
-    return string.format("[%s] ", parse_bin_path(program_name, "/"))
+    return string.format("[%s] ", helpers.parse_bin_path(program_name))
   end
 )
+
+-- Set the custom key bindings.
+config.keys = require("keys")
+config.colors = require("kanagawa_dragon")
+config.tab_bar_at_bottom = true
+config.audible_bell = "Disabled"
+config.use_fancy_tab_bar = false
+config.enable_scroll_bar = false
+config.freetype_load_flags = "NO_HINTING"
+config.font_size = 14
+
+config.font = wezterm.font({
+  family = "JetBrainsMono",
+  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+  weight = "Medium"
+})
+
+-- Set the window padding to zero.
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
+-- Do not dim inactive panes.
+config.inactive_pane_hsb = {
+  saturation = 1.0,
+  brightness = 1.0,
+}
 
 return config
